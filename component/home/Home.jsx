72976ac33156c4a1062181070ca09ab8d8691a97
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-01-21 22:15:34 +0100

    feat: FAQ section

--- component/home/Home.jsx
+++ component/home/Home.jsx
@@ -59,6 +59,41 @@ const Home = () => {
     },
   ];
 
+  const faqs = [
+    {
+      question: "How many participants can join a ClearLink video conference?",
+      answer:
+        "ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scale webinars, ensuring you have the right fit for your needs.",
+    },
+    {
+      question: "Can I use ClearLink on multiple devices?",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt voluptate dicta quo officiis explicabo consequuntur distinctio corporis earum similique!",
+    },
+    {
+      question:
+        "Is ClearLink compatible with other video conferencing platforms?",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt voluptate dicta quo officiis explicabo consequuntur distinctio corporis earum similique!",
+    },
+    {
+      question:
+        "How does ClearLink ensure the security of my video conferences?",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt voluptate dicta quo officiis explicabo consequuntur distinctio corporis earum similique!",
+    },
+    {
+      question: "Do I need to download any software to use ClearLink?",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt voluptate dicta quo officiis explicabo consequuntur distinctio corporis earum similique!",
+    },
+    {
+      question: "What kind of customer support does ClearLink provide?",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt voluptate dicta quo officiis explicabo consequuntur distinctio corporis earum similique!",
+    },
+  ];
+
   return (
     <section>
       {/* hero */}
@@ -204,7 +239,7 @@ const Home = () => {
       </div>
 
       {/* CTA section */}
-      <div className=" max-w-screen-xl mx-auto px-4 md:px-8 lg:px-4 py-24 space-y-20">
+      {/* <div className=" max-w-screen-xl mx-auto px-4 md:px-8 lg:px-4 py-24 space-y-20">
         <div className="flex justify-between gap-8">
           <div className="flex-1">
             <h2 className="text-[#101828] text-5xl font-semibold">
@@ -213,6 +248,74 @@ const Home = () => {
           </div>
           <div className="flex-none hidden lg:block">
             <Image src={Mockup} className="w-max" />
+          </div>
+        </div>
+      </div> */}
+
+      {/* FAQ */}
+      <div className="max-w-screen-xl mx-auto px-4 py-28">
+        <div className="lg:flex gap-16 justify-between">
+          <div className="flex-">
+            <div className="space-y-4">
+              <p className="text-[#175CD3] text-lg font-semibold">Support</p>
+              <h2 className="text-[#1D2939] text-5xl font-semibold">FAQs</h2>
+              <p className="text-[#667085] max-w-sm">
+                Everything you need to know about the product and billing. Can’t
+                find the answer you’re looking for? Please{" "}
+                <Link href="/" className="underline">
+                  chat to our friendly team.
+                </Link>
+              </p>
+            </div>
+          </div>
+          <div className="flex-1 mt-10 divide-y divide-gray-100 rounded-xl border border-[#EAECF0] bg-[#F9FAFB]">
+            {faqs.map((faq, index) => (
+              <details
+                key={index}
+                className="group p-6 [&_summary::-webkit-details-marker]:hidden bg-[#F9FAFB]"
+                open={index === 0} // Open only the first FAQ by default
+              >
+                <summary className="flex cursor-pointer items-center justify-between gap-1.5 text-[#101828]">
+                  <h2 className="text-xl font-medium">{faq.question}</h2>
+
+                  <span className="relative h-5 w-5 shrink-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </span>
+                </summary>
+
+                <p className="mt-4 leading-relaxed text-[#475467] text-lg">
+                  {faq.answer}
+                </p>
+              </details>
+            ))}
           </div>
         </div>
       </div>
